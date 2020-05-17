2c227d7a407fa314c84eddd1538f03676455b436
Joe Osborne <3324453+ettmetal@users.noreply.github.com>
2020-05-17 15:06:15 +0200

    Add text to BurstPool editor to remind that it will cause instantiation

--- Assets/Replicator/Editor/BurstPoolEditor.cs
+++ Assets/Replicator/Editor/BurstPoolEditor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 namespace Replicator.Editor {
 	[CustomEditor(typeof(BurstPool), true)]
 	public class BurstPoolEditor : ObjectPoolEditor {
+		private const string warningMessage = "Burst pools are designed to meet burst needs by instantiating, and culling objects. This will cause both instantiation, and destructions of GameObjects.";
+		protected override void beforeFields() => EditorGUILayout.HelpBox(warningMessage, MessageType.Warning);
 		protected override void growthField(SerializedProperty growth) {
 			GUIContent label = new GUIContent(growth.displayName, growth.tooltip);
 			GrowthStrategy current = (GrowthStrategy)growth.enumValueIndex;
