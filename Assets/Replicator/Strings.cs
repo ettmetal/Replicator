38293fbe8dc54e7b58dd9ceadcb1ff0e7b99736a
Joe Osborne <3324453+ettmetal@users.noreply.github.com>
2020-06-13 15:44:14 +0200

    Group all pools into a sub-menu. Closes #10

--- Assets/Replicator/Strings.cs
+++ Assets/Replicator/Strings.cs
@@ -5,7 +5,7 @@
 		public const string PreLoadTooltip = "The number of objects to pre-load. If set to 0, all objects will be instantiated as needed. Cannot be higher than capacity.";
 		public const string CapacityTooltip = "The maximum capacity of the pool.";
 		public const string GrowTooltip = "Should the pool be allowed to grow? When it reaches capcity, new instances will be created depending on the selected strategy, e.g. Half adds half the capacity again, Double adds the whole capacity again.";
-		public const string PoolMenuName = "Object Pool";
+		public const string PoolMenuName = "Object Pools/Object Pool";
 		public const string PoolFileName = "New Object Pool";
 		public const string CantRecycleFormat = "It is not possible to recyle this GameObject. {0}";
 		public const string NotPooled = "It does not belong to a pool.";
@@ -14,11 +14,11 @@
 		public const string HideUnspawedTooltip = "Should unspawned objects from this pool be visible in the hierarchy?";
 		#endregion
 		#region VARIANT_POOLS
-		public const string RandomPoolMenuName = "Random Pool";
-		public const string SequentialPoolMenuName = "Sequential Pool";
+		public const string RandomPoolMenuName = "Object Pools/Random Pool";
+		public const string SequentialPoolMenuName = "Object Pools/Sequential Pool";
 		#endregion
 		#region BURST_POOL
-		public const string BurstPoolMenuName = "Burst Pool";
+		public const string BurstPoolMenuName = "Object Pools/Burst Pool";
 		public const string BurstPoolCullMaxTooltip = "The maximum number of instances to cull in any frame";
 		public const string BurstPoolCullIntervalTooltip = "The interval, in seconds, to wait between culling expired instances";
 		#endregion
