09ef36a89d0da3e046d85a6440206981f94e9682
Joe Osborne <3324453+ettmetal@users.noreply.github.com>
2020-08-20 20:08:34 +0200

    Add a factory method for RandomPool

--- Assets/Replicator/RandomPool.cs
+++ Assets/Replicator/RandomPool.cs
@@ -4,8 +4,15 @@ namespace Replicator {
 	/// <summary>A multi-object pool which will return a random variant each time a spawn is requested</summary>
 	[CreateAssetMenu(menuName = Strings.RandomPoolMenuName, fileName = Strings.PoolFileName, order = 204)]
 	public class RandomPool : VariantPool {
-		protected override int GetSpawnIndex(int[] availableVariantIndecies) {
-			return availableVariantIndecies[Random.Range(0, availableVariantIndecies.Length)];
+		/// <summary>
+		/// Create a new <see cref="RandomPool"/> with the given parameters.
+		/// </summary>
+		public static RandomPool Create(GameObject[] allVariants, ushort capacity = 0, ushort preLoad = 0, GrowthStrategy growth = 0) {
+			RandomPool newPool = CreateInstance<RandomPool>();
+			newPool.Initialise(allVariants, capacity, preLoad, growth);
+			return newPool;
 		}
+		protected override int GetSpawnIndex(int[] availableVariantIndecies) =>
+			availableVariantIndecies[Random.Range(0, availableVariantIndecies.Length)];
 	}
 }
